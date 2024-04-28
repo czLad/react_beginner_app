3422cfbba45db993b49c3a496cfe1e4c95630925
Min Phone Myat Zaw <myatzawowl@gmail.com>
2024-04-28 14:50:12 -0700

    Tutorial 1:00:06

--- react-beginner-app-project/src/App.tsx
+++ react-beginner-app-project/src/App.tsx
@@ -1,9 +1,21 @@
 //import Message from './Message';
 import ListGroup from "./components/ListGroup";
 
-function App(){
+function App() {
+  let city_items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  let food_items = ["NY Hot Dog", "Gotham Food", "Sashimi", "English Muffins", "Croissant"];
   //<Message ></Message> == <Message /> (called self closing component syntax)
-  return <div><ListGroup /></div>;
+  // convention for event handler funcs is to start with handleEventName
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
+  return (
+    <div>
+      <ListGroup items={city_items} heading="Cities" onSelectItem={handleSelectItem} />
+      <ListGroup items={food_items} heading="Foods" onSelectItem={handleSelectItem} />
+    </div>
+  );
 }
 
 //Have to export react components to be used
--- react-beginner-app-project/src/components/ListGroup.tsx
+++ react-beginner-app-project/src/components/ListGroup.tsx
@@ -1,8 +1,35 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
+import { MouseEvent } from "react";
+//to dyanmically pass ListGroup objs into the jsx markup block
+//pass an obj with two props : {items: [], heading: string}
+// interface ListGroupProps for exactness but by convention
+// we use this v
+interface Props {
+  items: string[];
+  heading: string;
+  // (item: string) => void 
+  //^ means () function takes string as arg and return(=>) void
+  onSelectItem: (item: string) => void;
+}
 
-function ListGroup() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  items = [];
+// function ListGroup(props: Props) {
+// destructure ^ to this v
+function ListGroup({ items, heading, onSelectItem }: Props) {
+  // let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  // ^ has turned dyanmic
+
+  // v Hook - can tap into built in features of react
+  // const arr = useState(-1); //arr has two elements
+  //arr[0] //variable (selectedIndex)
+  //arr[1] //updater function
+  // ^ instead of working with two individual elements, destructure arr
+  // into two elements like this v
+  //v structure: const [stateVariable, setFunctionForThatStateVariable]
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  //Event Handler
+  // const handleClick = (event: MouseEvent) => console.log(event)
+
   //v this code works like a for loop and shows all elements in items array
   //items.map((item) => <li>{item}</li>);
 
@@ -33,14 +60,37 @@ function ListGroup() {
     //because ^ gets translated to javascript e.g.  React.createElement('h1');
     //v this means by default <Fragment>
     <>
-      <h1>List</h1>
+      {/* <h1>List</h1> has turned dynamic v */}
+      <h1>{heading}</h1>
 
       {/* {items.length == 0 ? <p>No items found</p> : null} */}
       {/* v shorter form of ^ a.k.a short circuiting*/}
       {items.length == 0 && <p>No items found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            // className="list-group-item active"
+            //^ Not highlighting only active index
+            //v to highlight only active index //also v adding classes dynamically
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            //onClick={(event) => console.log(event)}
+            //v tidied up version of this^
+            // onClick={handleClick}
+            //^ static, to have dynamic management of active state,
+            //v pass in an arrow function and define with
+            //the set function of the state variable
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
